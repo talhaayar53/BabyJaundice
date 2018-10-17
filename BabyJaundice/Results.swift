7406d06065f4387f23a409c5cd16fec71a338995
TALHA AYAR <talhaayar@TALHA-MacBook-Air.local>
2018-10-17 11:01:33 +0300

    first commit

---
+++ BabyJaundice/Results.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  BabyJaundice
+//
+//  Created by TALHA AYAR on 12.10.2018.
+//  Copyright © 2018 TALHA AYAR. All rights reserved.
+//
+
+import Foundation
---
+++ BabyJaundice/SideTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SideTableViewCell.swift
+//  BabyJaundice
+//
+//  Created by TALHA AYAR on 12.10.2018.
+//  Copyright © 2018 TALHA AYAR. All rights reserved.
+//
+
+import UIKit
+
+class SideTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
